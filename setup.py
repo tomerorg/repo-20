e7c80e8c2178a37c9ebc568dde4af188c6853a32
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:55:26 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-20",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="A Python ai implementation using Celery and Flask and FastAPI",
+    keywords="repo-20, python",
+    url="",
+)
